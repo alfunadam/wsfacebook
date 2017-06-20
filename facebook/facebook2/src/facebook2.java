aea9f29ed81831127e2d6ffcaab6e2e2031c7177
alfunadam <alfun_a@mhs.unisbank.ac.id>
2017-06-20 23:46:47 +0700

    Add files via upload

---
+++ facebook/facebook2/src/facebook2.java
@@ -0,0 +1,17 @@
+/**
+ * @(#)facebook2.java
+ *
+ * facebook2 application
+ *
+ * @author 
+ * @version 1.00 2017/6/13
+ */
+ 
+public class facebook2 {
+    
+    public static void main(String[] args) {
+    	
+    	// TODO, add your application code
+    	System.out.println("Hello World!");
+    }
+}
